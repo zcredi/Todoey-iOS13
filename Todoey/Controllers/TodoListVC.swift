2589c9996e4d24df45209da235ce7fb51c0028b1
zcredi <zcredi@gmail.com>
2023-01-05 15:13:52 +0300

    add Encoding Data with NSCoder

--- Todoey/Controllers/TodoListVC.swift
+++ Todoey/Controllers/TodoListVC.swift
@@ -10,26 +10,53 @@ import UIKit
 
 class TodoListVC: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Demogorgon"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathExtension("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "He He"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Hey! Good!"
+        itemArray.append(newItem3)
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
+        
     }
     
     //MARK: - Tableview Datasourse Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
     
@@ -39,12 +66,11 @@ class TodoListVC: UITableViewController {
         // Unselect the row.
         tableView.deselectRow(at: indexPath, animated: true)
         
-        // Mark the newly selected filter item with a checkmark.
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        // Галочка, проверяет ячейку лож или правда и меняет на противоположное
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
     }
     
     //MARK: - Add New Item
@@ -59,11 +85,14 @@ class TodoListVC: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // что произойдет, когда пользователь нажмет кнопку Добавить элемент в нашем пользовательском интерфейсе
-            self.itemArray.append(textField.text!)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.tableView.reloadData()
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
         }
         
         alert.addTextField { (alertTextField) in
@@ -74,5 +103,21 @@ class TodoListVC: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true)
         
+    }
+    
+    //MARK: - Model Manupulation Methods
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+         
+         do {
+             let data = try encoder.encode(itemArray)
+             try data.write(to: dataFilePath!)
+         } catch {
+             print("Error encoder \(error)")
+         }
+         
+         self.tableView.reloadData()
     }
 }
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Владислав on 04.01.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item: Encodable {
+    
+    var title = ""
+    var done = false
+    
+}
